e352d15e72813d2423297f823232d230886315b3
Michael Lauridsen <codewarriorx@gmail.com>
2013-05-04 15:27:38 -0400

    Misc

--- client/logReg/events.js
+++ client/logReg/events.js
@@ -28,50 +28,73 @@ Template.loginForm.events = {
 	}
 };
 
+Template.chat.events = {
+	'submit': function(e, tpl){
+		e.preventDefault();
+
+		var message = tpl.find('#message').value;
+
+		if(message != ""){
+			MainChat.insert({username: Session.get('credentials').username, message: message, timestamp: Date.now()});
+			tpl.find('#message').value = '';
+		}
+		else{
+			alert('You cannot enter an empty message.');
+		}
+	}
+};
+
+Template.userList.events = {
+	'click a': function(e, tpl){
+		e.preventDefault();
+
+		console.log('clicked challenge: ');
+		console.log(this);
+		alert('Challenge: '+this.name);
+
+		var id = Meteor.call('getIdFromToken','03ace924c7e74a59e5a4f790b8542845f6863aba6d696b65osccJEAGhDCyFLw5t6g118h45ee', function(err, result){
+			console.log(result);
+		});
+
+		// Challenges.insert({ challenger: , challengee: , timestamp: Date.now() })
+	}
+};
+
 Template.regForm.events = {
 	'submit': function(e, tpl){
 		e.preventDefault();
 
+		// reset any existing errors
+		Meteor.call('clearRegErrors');
+
 		// get user input
-		var username = tpl.find('input[name="username"]').value;
-		var password = tpl.find('input[name="password"]').value.trim();
-		var password2 = tpl.find('input[name="password2"]').value.trim();
+		var details = {
+			username: tpl.find('input[name="username"]').value,
+			password: tpl.find('input[name="password"]').value.trim(),
+			password2: tpl.find('input[name="password2"]').value.trim()
+		}
 
 		// get users ip address !!!!! this doesn't work
 		// Meteor.call('getIP', Session.get('this'));
 
-		// check username for duplicate
-		var cursor = Users.find({username: username});
-		if(cursor.count() > 0){ // does name exists, display error
-			Meteor.call('setError', 'usernameExists', true);
-		}
-		else{
-			Meteor.call('setError', 'usernameExists', false);
-
-			if(password == password2 && (password != "" && password2 != "")){ // check password match and are not empty
-				Meteor.call('setError', 'passwordMatch', false);
-
-				// emter into the database since it passed both checks
-				var reg = {
-					username: username,
-					password: password
-				};
-
-				Meteor.call('registerUser', reg, function(err, result){
-					if(err){
-						alert('Could not register '+ err.reason);
-					}
-
-					if(result){
-						// log user in
-						Session.set('userName', reg.username);
-						localStorage.username = reg.username;
-					}
-				});
+		// try to register
+		Meteor.call('registerUser', details, function(err, result){
+			if(err){
+				alert(err.reason);
+				switch(err.error) {
+					case 401:
+						Meteor.call('setError', 'fieldsAreBlank', true);
+				        break;
+					case 402:
+				        Meteor.call('setError', 'passwordMatch', true);
+				        break;
+		        	case 403:
+		                Meteor.call('setError', 'usernameExists', true);
+		                break;
+				}
+				// use error code to set reg form error message
 			}
-			else{
-				Meteor.call('setError', 'passwordMatch', true);
-			}
-		}
+			
+		});
 	}
 };
